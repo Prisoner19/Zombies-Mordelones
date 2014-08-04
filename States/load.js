f430ef8ec27d34441980e972954314b21adb2938
Germán Cruz <clz.german@gmail.com>
2014-08-04 16:34:38 -0500

    Scenes added

--- States/load.js
+++ States/load.js
@@ -5,6 +5,7 @@ var load_state =
         game.stage.backgroundColor = "#3D3632";
         game.load.spritesheet('button', 'Resources/Graphics/spritesheet.png', 333, 36); 
         game.load.text('game_data', 'Resources/JSON/Escenas.json');
+        game.load.audio('zombie', "Resources/Sound/zombie.ogg");
     },
     
     create: function ()
